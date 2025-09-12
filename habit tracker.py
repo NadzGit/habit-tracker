004fb990f2055a647f8bf3b190a45f27de9cac41
NadzGit <nadineseddat@gmail.com>
2025-09-12 22:18:06 +0100

    I have made the following changes to the habit tracker application: I have added a button that, when clicked, prints "{habit} Habit Tracked!" to the console. {habit} being the name of the habit being tracked.

--- habit tracker.py
+++ habit tracker.py
@@ -2,22 +2,30 @@ import tkinter as tk
 
 # test click event
 def on_click():
-    print("Habit Tracked!")
+    habit = add_habit.get()
+    print(f"{habit} Habit Tracked!")
+    add_habit.delete(0, tk.END)
 
 
 root = tk.Tk()
 root.geometry("500x500")
-canvas = tk.Canvas(root, height=500, width=500)
-canvas.grid(row=0, column=0)
-canvas.create_rectangle(2,0,500,100,fill="lightblue") #position will change lol
+root.configure(bg="#8CCDD4")
+canvas = tk.Canvas(root, height=500, width=500,highlightthickness=0,)
+canvas.configure(bg= "#8CCDD4")
+canvas.grid(row=0, column=0, sticky="nsew")
+canvas.create_rectangle(2,0,500,100,fill="#4CB5E8",) #position will change lol
 
 root.title("Habit Tracker")
 title = tk.Label(root, text="dini's habit tracker", font=("Arial", 10), bg="lightblue", fg="white")
 welcome = tk.Label(root, text="welcome back!", font=("Arial", 10), bg="lightblue", fg="white")
 title.place(x=200, y=20)
 welcome.place(x=205, y=50)  
-# btn = tk.Button(root, text="Track Habit", command=on_click)
-# btn.grid(row=0, column=1)
+
+add_habit = tk.Entry(root, font=("Arial", 10), bg="white", fg="black")
+add_habit.place(x=200, y=130)
+
+btn = tk.Button(root, text="Add", command=on_click)
+btn.place(x=200, y=200)
 
 
 
