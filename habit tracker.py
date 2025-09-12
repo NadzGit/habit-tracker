9bfb359e8008faa41238bb60825faf22961960a9
NadzGit <nadineseddat@gmail.com>
2025-09-12 21:53:00 +0100

    Repositioned title message in GUI and added colour.

--- habit tracker.py
+++ habit tracker.py
@@ -1,11 +1,25 @@
 import tkinter as tk
 
+# test click event
+def on_click():
+    print("Habit Tracked!")
+
+
 root = tk.Tk()
+root.geometry("500x500")
+canvas = tk.Canvas(root, height=500, width=500)
+canvas.grid(row=0, column=0)
+canvas.create_rectangle(2,0,500,100,fill="lightblue") #position will change lol
+
 root.title("Habit Tracker")
-label = tk.Label(root, text="Welcome to Habit Tracker")
-label.grid(row=0, column=0)
-btn = tk.Button(root, text="Track Habit")
-btn.grid(row=0, column=1)
+title = tk.Label(root, text="dini's habit tracker", font=("Arial", 10), bg="lightblue", fg="white")
+welcome = tk.Label(root, text="welcome back!", font=("Arial", 10), bg="lightblue", fg="white")
+title.place(x=200, y=20)
+welcome.place(x=205, y=50)  
+# btn = tk.Button(root, text="Track Habit", command=on_click)
+# btn.grid(row=0, column=1)
+
+
 
 
 root.mainloop()
