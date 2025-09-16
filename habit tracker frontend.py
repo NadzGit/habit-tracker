85a0bb2aa63bf97fc0e0110c74f243716ffcac03
NadzGit <nadineseddat@gmail.com>
2025-09-16 18:55:58 +0100

    Much better formatting (given a 1920x1080 screen) as well as added aesthetic of an appropriately sized and coloured add button :)

--- habit tracker frontend.py
+++ habit tracker frontend.py
@@ -3,24 +3,27 @@ from habittrackerbackend import *
 
 
 root = tk.Tk()
-root.geometry("500x500")
-root.configure(bg="#8CCDD4")
-canvas = tk.Canvas(root, height=500, width=500,highlightthickness=0,)
-canvas.configure(bg= "#8CCDD4")
-canvas.grid(row=0, column=0, sticky="nsew")
-canvas.create_rectangle(2,0,500,100,fill="#4CB5E8",) #position will change lol
+root.geometry("1920x1080")
+
+# welcome rectangle widget
+frame = tk.Frame(width = 885, height= 266, bg = "#4CB5E8")
+frame.pack(side = "top")
+
 
 root.title("Habit Tracker")
-title = tk.Label(root, text="dini's habit tracker", font=("Arial", 10), bg="lightblue", fg="white")
-welcome = tk.Label(root, text="welcome back!", font=("Arial", 10), bg="lightblue", fg="white")
-title.place(x=200, y=20)
-welcome.place(x=205, y=50)  
+title = tk.Label(root, text="dini's habit tracker", font=("Arial", 10), bg = "#4CB5E8",fg="white")
+welcome = tk.Label(frame, text="welcome back!", font=("Arial", 10),bg = "#4CB5E8",fg="white")
+title.place(x=894, y=78)
+welcome.place(x=385, y=118)  
+
 
 add_habit = tk.Entry(root, font=("Arial", 10), bg="white", fg="black")
-add_habit.place(x=200, y=130)
+add_habit.place(x=600, y=163, width = 340, height = 54)
 
 btn = tk.Button(root, text="Add", command=lambda:on_click(add_habit, tk))
-btn.place(x=200, y=200)
+btn.config(bg = "#F8DAE7")
+btn.place(x=953, y=163, width = 340, height = 54)
+
 
 
 
